5626fe26cddadc8d7692cf72e84353e0e3bcf0fd
jmo5896 <justin.m.moore5896@gmail.com>
2019-12-18 20:15:21 -0600

    click events work, set up outline functions for api calls, set up git ignore and my_key file

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,33 @@
 $(document).ready(() => {
-console.log('jquery is loaded');
-let cities = []
-$('#submit-btn').on('click', function(event){
-    event.preventDefault();
-    var myInput = $("#myCity").val();
-    if (cities.includes(myInput)){
-        alert('City has already been entered')
-    } else {
-        $('#myCities').append(`<li class="li">${myInput} </li>`)
-        cities.push(myInput);
-    }
-    $("#myCity").val("")
-    console.log(myInput)
-});
+    console.log('jquery is loaded');
+    let cities = []
 
-$("#myCities").on('click',".li", function(){
-    console.log($(this).text());
-})
+    function dailyWeather(city) {
+        console.log(city);
+
+    }
+
+    function fiveDay(city) {
+        console.log(city);
+    }
+
+    $('#submit-btn').on('click', function (event) {
+        event.preventDefault();
+        var myInput = $("#myCity").val();
+        if (cities.includes(myInput)) {
+            alert('City has already been entered')
+        } else {
+            $('#myCities').append(`<li class="li">${myInput} </li>`)
+            cities.push(myInput);
+        }
+        $("#myCity").val("")
+
+        dailyWeather(myInput);
+        fiveDay(myInput);
+    });
+
+    $("#myCities").on('click', ".li", function () {
+        console.log($(this).text());
+    })
 
 });
