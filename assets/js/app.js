1cc1f13850fcdf219ea2c6c2755571e03ff0c4e2
jmo5896 <justin.m.moore5896@gmail.com>
2019-12-31 12:12:10 -0600

    finally figured out how to implement my filter and I was overthinking of course.  All I had to do was split the str date and filter by the time (I chose 15:00:00).

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,33 +4,40 @@ $(document).ready(() => {
 
     function dailyWeather(city) {
         const url = `http://api.openweathermap.org/data/2.5/weather?APPID=${API_KEY}&q=${city}&units=imperial`
-        $.get(url, function(data){
+        $.get(url, function (data) {
             // console.log(data)
             const weather = {
-                Name: data.name, 
+                Name: data.name,
                 Temperature: data.main.temp,
                 Humidity: data.main.humidity,
                 Overcast: data.weather[0].description
             };
-            
+
             for (let key in weather) {
                 let item = $('#dailyWeather').append(`<li>${key}: ${weather[key]}</li>`);
             }
-          });
+        });
 
     }
 
     function fiveDay(city) {
         const url = `http://api.openweathermap.org/data/2.5/forecast?APPID=${API_KEY}&q=${city}&units=imperial`
-        $.get(url, function(data){
-            const days = data.list            
-            let myDays = days.map(obj =>{
-                const curTime = moment(obj.dt, "X").format("MM-DD-YYYY");
-                obj.dt = curTime;
-                return obj;               
-            });
-            console.log(myDays)
-          });
+        $.get(url, function (data) {
+            $("#fiveDay").empty()
+            let forcast = data.list.filter(obj => obj.dt_txt.split(" ")[1] === "15:00:00");
+            console.log(forcast);
+            forcast.forEach(obj => {
+                $("#fiveDay").append(`
+                    <div class="card" style="max-width: 18rem;">
+                        <div class="card-body">
+                            <p class="card-text">temp: ${obj.main.temp}</p>
+                            <p class="card-text"> hum: ${obj.main.humidity}</p>
+                            <p class="card-text"> Overcast: ${obj.weather[0].description}</p>
+                        </div>
+                    </div>
+               `)
+            })
+        });
     }
 
     $('#submit-btn').on('click', function (event) {
